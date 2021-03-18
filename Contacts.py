721b8aa22ce5233478687393a53343ee7c12f0c2
Mariusz Perzynski <m.perzyk@interia.pl>
2021-03-18 12:18:16 +0100

    add text with enter

--- Contacts.py
+++ Contacts.py
@@ -1,4 +1,4 @@
-plik = open("kontakty.txt","a")
+plik = open("kontakty.txt", "a")
 
 if plik.writable():
 
@@ -6,12 +6,12 @@ if plik.writable():
     plik.write(input("Nazwisko: ") + "\n")
     plik.write(input("Narodowość: ") + "\n")
     plik.write(input("Telefon: ") + "\n")
-    plik.write(input("<==============================> ( ENTER --> ZATWIERDZ )") + "\n")
+    plik.write(input("( ENTER --> ZATWIERDZ )") + "\n")
 
 plik.close()
 
 
-plik = open("kontakty.txt","r")
+plik = open("kontakty.txt", "r")
 
 if plik.readable():
 
